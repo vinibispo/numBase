d5fb3f88e5fef21a1e4e62e6932295603a532041
vinibispo <vini.bispo015@gmail.com>
2020-02-13 05:47:20 -0300

    removing eslint and prettier

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
-import React from 'react'
-import {Text, Button, View} from 'react-native'
-export default App = () =>{
+/* eslint-disable keyword-spacing */
+/* eslint-disable semi */
+/* eslint-disable prettier/prettier */
+import React, { useEffect, useState } from 'react'
+import {Text, View, TextInput, StyleSheet} from 'react-native'
+const App = () =>{
+const [number, setNumber] = useState(0)
+  useEffect(()=>{
+    setNumber(number)
+  }, [number])
   return(
-    <View>
-      <Text>Bem vindo!</Text>
-      <Button onPress={() => alert('Você clicou em mim')}>Clique em mim</Button>
+    <View style ={styles.container}>
+      <Text style={styles.title}>Conversor</Text>
+      <TextInput placeholderTextColor={"#0abcde"} style={styles.number} placeholder={"Número"} keyboardType={"numeric"}></TextInput>
+      <TextInput placeholderTextColor={"#0abcde"} style={styles.number} placeholder={"Binário"} keyboardType={"numeric"}>{number}</TextInput>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#000',
+    flex: 1,
+  },
+  title:{
+    fontSize: 48,
+    color: "#fff",
+    textAlign: "center"
+  },
+  number:{
+    fontWeight: "bold",
+    fontSize: 30,
+    color: "#0abcde",
+  }
+})
+export default App
