17986e186095c430c307e2b3d422dc98328869d8
vinibispo <vini.bispo015@gmail.com>
2020-02-14 16:29:10 -0300

    adding icon

--- App.js
+++
@@ -1,9 +0,0 @@
-import React, {useState, useEffect} from 'react'
-import App from './src/App'
-import Loading from './src/screen/Loading'
-export default First = () =>{
-    const [screen, setScreen] = useState('Splash')
-    return(
-        screen === 'Splash' ? <Loading/> : <App/> 
-    )
-}
