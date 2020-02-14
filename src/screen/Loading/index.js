3f42d0af8a3219f5de8d6f796bab72b5eac30b6f
vinibispo <vini.bispo015@gmail.com>
2020-02-14 10:22:35 -0300

    finished Loading basic

---
+++ src/screen/Loading/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {View, StyleSheet} from 'react-native'
+
+export default Loading = () =>{
+    return(
+        <View style={styles.container}>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#000'
+    },
+})
